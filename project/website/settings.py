118f9da1d0a8d0c2fa450cf2af856fd0b4c33c51
mysocksdontfit <idkjacob772@gmail.com>
2026-09-01 22:01:07 +0100

    created a forms file

--- project/website/settings.py
+++ project/website/settings.py
@@ -17,7 +17,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+STATIC_ROOT = BASE_DIR / STATIC_URL
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
