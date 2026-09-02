3fce9b7a3fb9a86e20a3067994617391700bc7e0
mysocksdontfit <idkjacob772@gmail.com>
2026-09-02 17:26:53 +0100

    Successfully added HTMX alert when a user creates a post

--- project/app/views.py
+++ project/app/views.py
@@ -5,34 +5,32 @@ from django.contrib.auth.models import User
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from app.decorators import user_can_post
+from django.views.decorators.http import require_http_methods
 
 def index(request):
     return render(request, "app/index.html")
 
 @login_required
-@user_can_post # custom dec
+# @user_can_post # custom decorator 
 def create_post(request):
 
     current_user = User.objects.get(id=request.user.id)
-    user_profile = UserProfile.objects.get_or_create(user=current_user)
+    UserProfile.objects.get_or_create(user=current_user) # create user_profile instance  
 
     context = {}
     form = PostForm()
-    context["form"] = form
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
              new_post = form.save(commit=False)
-             
              new_post.user_id = UserProfile.objects.get(user=current_user).id
-             print(new_post.user)
              form.save()
+             return render(request, "app/create_post.html#success-message", context)
 
-
-            #  return redirect("index")
-        else:
-            print("ffs")
+    context["form"] = form
 
     return render(request, "app/create_post.html", context)
 
-    
+@login_required
+def user_posts(request):
+    pass
